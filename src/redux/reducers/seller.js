16261ce26fdb3a10b8130bf536c4624fd2bd01a7
Edwin Rodriguez <edwinrodriguez@Edwins-MBP.attlocal.net>
2023-04-28 10:05:06 -0500

    seller protected route error fixed

--- src/redux/reducers/seller.js
+++ src/redux/reducers/seller.js
@@ -1,7 +1,7 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
-  isAuthenticated: false,
+    isLoading: false,
 };
 
 export const sellerReducer = createReducer(initialState, {
@@ -9,14 +9,14 @@ export const sellerReducer = createReducer(initialState, {
       state.isLoading = true;
     },
     LoadSellerSuccess: (state, action) => {
-      state.isAuthenticated = true;
+      state.isSeller = true;
       state.isLoading = false;
       state.seller = action.payload;
     },
     LoadSellerFail: (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
-      state.isAuthenticated = false;
+      state.isSeller = false;
     },
     clearErrors: (state) => {
       state.error = null;
