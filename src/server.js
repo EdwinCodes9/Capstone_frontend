77632db6caddd8970109ce5932601496df45796f
Edwin Rodriguez <edwinrodriguez@Edwins-MBP.attlocal.net>
2023-05-09 14:57:32 -0500

    server update

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://e-com-back-end.vercel.app/api/v2";
+export const server = "https://e-com-back-kr37okd67-edwincodes9.vercel.app/api/v2";
 
-export const backend_url = "https://e-com-back-end.vercel.app/";
+export const backend_url = "https://e-com-back-kr37okd67-edwincodes9.vercel.app/";
